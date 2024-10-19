489e20a11a391ec96e56f396172d1957a0174ed3
José Junior <junior.devstack@gmail.com>
2024-10-19 11:38:24 -0300

    Logo de clientes.

--- resources/views/layouts/comment_clients.blade.php
+++ resources/views/layouts/comment_clients.blade.php
@@ -1,93 +1,73 @@
 <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
     <div class="container">
         <div class="text-center">
-            <h6 class="text-secondary text-uppercase">Comentários</h6>
-            <h1 class="mb-5">O que dizem os nossos clientes!</h1>
+            <h1 class="mb-5">Nossos clientes!</h1>
         </div>
         <div class="owl-carousel testimonial-carousel position-relative wow fadeInUp" data-wow-delay="0.1s">
             <div class="testimonial-item text-center">
-                <div class="testimonial-text bg-light text-center p-4 mb-4">
-                    <p class="mb-0">Profissionais extremamente qualificados no que são designados.</p>
-                </div>
-                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/testimonial-1.jpg"
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/amo_saude.jpeg"
                     style="width: 80px; height: 80px;">
-                <div class="mb-2">
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                </div>
-                <h5 class="mb-1">...</h5>
-                <p class="m-0">...</p>
+                <h5 class="mb-1">Amor Saúde</h5>
             </div>
             <div class="testimonial-item text-center">
-                <div class="testimonial-text bg-light text-center p-4 mb-4">
-                    <p class="mb-0">Empresa extremamente empenhada em atender as necessidades, satisfação completa com
-                        os serviços prestados.</p>
-                </div>
-                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/testimonial-2.jpg"
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/lecard.jpeg"
                     style="width: 80px; height: 80px;">
-                <div class="mb-2">
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                </div>
-                <h5 class="mb-1">...</h5>
-                <p class="m-0">...</p>
+                <h5 class="mb-1">Le Card</h5>
             </div>
             <div class="testimonial-item text-center">
-                <div class="testimonial-text bg-light text-center p-4 mb-4">
-                    <p class="mb-0">Empresa extremamente empenhada em atender as necessidades, satisfação completa com
-                        os serviços prestados.</p>
-                </div>
-                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/testimonial-2.jpg"
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/arquivo_pb.jpeg"
                     style="width: 80px; height: 80px;">
-                <div class="mb-2">
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                </div>
-                <h5 class="mb-1">...</h5>
-                <p class="m-0">...</p>
+                <h5 class="mb-1">Arquivo Público</h5>
             </div>
             <div class="testimonial-item text-center">
-                <div class="testimonial-text bg-light text-center p-4 mb-4">
-                    <p class="mb-0">Empresa extremamente empenhada em atender as necessidades, satisfação completa com
-                        os serviços prestados.</p>
-                </div>
-                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/testimonial-2.jpg"
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/beta.jpeg"
                     style="width: 80px; height: 80px;">
-                <div class="mb-2">
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                </div>
-                <h5 class="mb-1">...</h5>
-                <p class="m-0">...</p>
+                <h5 class="mb-1">Beta Rede</h5>
             </div>
             <div class="testimonial-item text-center">
-                <div class="testimonial-text bg-light text-center p-4 mb-4">
-                    <p class="mb-0">Empresa extremamente empenhada em atender as necessidades, satisfação completa com
-                        os serviços prestados.</p>
-                </div>
-                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/testimonial-2.jpg"
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/cam_car.jpeg"
                     style="width: 80px; height: 80px;">
-                <div class="mb-2">
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                    <small class="fa fa-star text-secondary"></small>
-                </div>
-                <h5 class="mb-1">...</h5>
-                <p class="m-0">...</p>
+                <h5 class="mb-1">Câmara Municipal de Cariacica</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/clube.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Clube dos Oficiais</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/genesis.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Gênesis</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/global_multi.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Global Multidisciplinar</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/mivita.png"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Mivita Construtora</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/pge.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Procuradoria Geral - ES</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/sec_es.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Secretaria de Ciência e Tecnologia - ES</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/vista.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Vista de Pitanga</h5>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="bg-light rounded-circle p-2 mx-auto mb-2" src="img/partners/wellness.jpeg"
+                    style="width: 80px; height: 80px;">
+                <h5 class="mb-1">Wellness Club</h5>
             </div>
         </div>
     </div>
